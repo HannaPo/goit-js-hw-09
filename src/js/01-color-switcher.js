17c537ff3722291d3a610f85d95a5433109c0257
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-03-18 18:33:07 +0200

    Task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+ function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+btnStart.addEventListener("click", (e) => {
+  e.target.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  
+  timerId = setInterval(() => {
+   body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener("click", (e) => {
+  e.target.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+
+  clearInterval(timerId);
+});
