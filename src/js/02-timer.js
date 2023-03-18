20e028e3d7422eb839b2aa663b28590dcf599f0d
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-03-18 22:40:28 +0200

    timer_styles

---
+++ src/js/02-timer.js
@@ -0,0 +1,21 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const dateTime = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+let timerId = null;
+
+flatpickr(dateTime, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+});
